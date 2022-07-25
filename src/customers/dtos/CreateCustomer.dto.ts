3ee10f1c4bb414c254225e80bc5f92aab25f7b1d
Stefan Vatasescu <stefan.vatasescu@global-technical.com>
2022-07-25 17:31:17 +0300

    WIP-4

--- src/customers/dtos/CreateCustomer.dto.ts
+++ src/customers/dtos/CreateCustomer.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsNumberString, IsString } from "class-validator";
+
 export class CreateCustomerDto {
+
+    @IsNumberString()
     id : number;
+
+    @IsEmail()
     email : string;
+
+    @IsNotEmpty()
     name : string;
 }
